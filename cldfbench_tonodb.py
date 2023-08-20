1444b0a64df04859e3c0d16563324fcbe1e445ee
Zifan <J22Melody@users.noreply.github.com>
2023-08-20 17:28:01 +0200

    update columns (#3)
    * update columns
    
    * update source typo

--- cldfbench_tonodb.py
+++ cldfbench_tonodb.py
@@ -31,7 +31,7 @@ class Dataset(BaseDataset):
     dir = pathlib.Path(__file__).parent
     id = "tonodb"
     # valueTableProperties = ['LanguageVariety', 'TriggeringContext' , 'Extra', 'Height', 'Contour', 'Phonation', 'ToneDescription', 'ChaoNumerals', 'Notes', 'EffectOnPitch', 'ResultantSystem', 'Type', 'Onset', 'Coda', 'Stress/quantity', 'Wordtype', 'long vowel']
-    valueTableProperties = ['LanguageVariety', 'TriggeringContext', 'Extra', 'Height', 'Contour', 'Phonation', 'ToneDescription' , 'ChaoNumerals', 'Notes', 'EffectOnPitch', 'ResultantSystem', 'Type', 'Onset', 'Coda', 'Stress/quantity', 'Wordtype', 'Nucleus']
+    valueTableProperties = ['LanguageVariety', 'TriggeringContext', 'Extra', 'Height', 'Contour', 'Phonation', 'ToneDescription' , 'ChaoNumerals', 'Notes', 'EffectOnPitch', 'ResultantSystem', 'Type', 'Onset', 'OnsetManner', 'OnsetVoicing', 'OnsetAspiration', 'Coda', 'CodaPhonation', 'CodaGlottal', 'CodaManner', 'Stress/quantity', 'NucleusATR', 'NucleusLength', 'NucleusHeight', 'Wordtype', 'Nucleus']
     languageTableProperties = ['family_id', 'parent_id', 'bookkeeping', 'level', 'description', 'markup_description', 'child_family_count', 'child_language_count', 'child_dialect_count', 'country_ids']
     inventoryTableProperties = ['LanguageVariety', 'Family', 'Area', 'Notes', 'BibTex']
 
